41d4155b3faf6599aea126fa77b7ca64575c5610
b-junyoung <157205701+b-junyoung@users.noreply.github.com>
2024-03-10 12:17:34 +0000

    Final

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 stdform = input('Enter a number in scientific notation: ')
-stdform = stdform.strip()
+stdform = stdform.strip("")
 
 # Type your code below
+x_pos = stdform.find("x")
+std_num = stdform[:x_pos]
+power_pos = stdform.find("^")
+power = stdform[power_pos + 1:]
+print(f"This number in E notation is {std_num}E{power}.")
